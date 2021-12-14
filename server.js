de0abc21fb4ced78152e9cfa1b5d878e97210d05
iakkm <90707822+iakkm@users.noreply.github.com>
2021-12-14 16:18:44 +0100

    sketch added

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 console.log("up and running");
+
+let express = require("express");
+
+let app = express();
+
+let port = 5500;
+
+let server = app.listen(port);
+
+console.log("server is running on http://localhost:" + port);
+
+app.use(express.static("public"));
